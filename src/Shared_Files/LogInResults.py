79ffee4d71746b4980f03fc335b8964eca257c44
Sebasthian Molina <2242541@slu.edu.ph>
2025-05-04 23:28:20 +0800

    Updated the Client_Python and the login results for testing

---
+++ src/Shared_Files/LogInResults.py
@@ -0,0 +1,6 @@
+# Shared_Files/player_account.py (or another file where appropriate)
+
+class LoginResult:
+    def __init__(self, session_token, player_account):
+        self.session_token = session_token
+        self.player_account = player_account
